7424fcd9ed26d4dcc15ea19fcb665ce515d97c4f
Russ Panula <dfadev@dfa.net>
2017-08-13 12:51:53 -0500

    fix indentation of config.js test

--- test/expected/config.js
+++ test/expected/config.js
@@ -6,12 +6,12 @@ var Sequelize = require("sequelize"),
     cfg = {
 		development: {
 				dialect: "sqlite",
-				storage: "./db.development.sqlite",
-				test: {
-						dialect: "sqlite",
-						storage: ":memory:",
-						logging: false
-				}
+				storage: "./db.development.sqlite"
+		},
+		test: {
+				dialect: "sqlite",
+				storage: ":memory:",
+				logging: false
 		}
 }[env],
     sequelize = new Sequelize(cfg);
--- test/src/config.js
+++ test/src/config.js
@@ -4,7 +4,7 @@ var db = SQLZINIT>
 			(development)
 					(dialect="sqlite")
 					(storage="./db.development.sqlite")
-				(test(dialect="sqlite", storage=":memory:"))
+			(test(dialect="sqlite", storage=":memory:"))
 						(logging=false)
 	// execution environment
 	(environment=process.env.NODE_ENV || "development")
