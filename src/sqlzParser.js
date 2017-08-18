c99d4137277f469cd7563f37e4aa1733462a87e6
Russ Panula <dfadev@dfa.net>
2017-08-18 15:13:56 -0500

    change order of function declarations

--- src/sqlzParser.js
+++ src/sqlzParser.js
@@ -2,92 +2,6 @@ import * as t from "babel-types";
 import { parseCalls, orderBlocks, chainElement } from "./firstPassParser";
 import { classesToOptions, attributesToOptions, childrenToOptions, objOrAssign, childrenToNamedOptions, generateIfChain } from "./secondPassParser";
 
-function parseAssociation(varName, assoc, topPath) {
-	let type = t.identifier(assoc.block.selector.tag),
-		target = t.memberExpression(
-			t.memberExpression(t.identifier("sequelize"), t.identifier('models')), 
-			t.identifier(assoc.block.selector.classes[0])),
-		options = t.objectExpression([]),
-		optionalOptions = [];
-
-	attributesToOptions(assoc, options);
-	childrenToOptions(assoc, options, optionalOptions, false, [], topPath);
-	let assocOpts = objOrAssign(options, optionalOptions);
-
-	let rslt = 
-		t.callExpression(
-			t.memberExpression(t.identifier(varName), type),
-			[ target, assocOpts ]);
-	return rslt;	
-}
-
-function parseAssociations(varName, child, associations, topPath) {
-
-	for (let i = 0; i < child.children.length; i++) {
-		let assoc = child.children[i];
-		let conditions = [];
-
-		if (assoc.block.type == "ElementBlock") {
-
-			associations.push(t.expressionStatement(parseAssociation(varName, assoc, topPath)));
-
-		} else if (assoc.block.type == "AssignmentExpression") {
-			let assocElement = orderBlocks([chainElement(assoc.block.path.right, topPath.hub.file)], topPath.hub.file)[0];
-			assocElement.children = assoc.children;
-
-			associations.push(t.expressionStatement(
-				t.assignmentExpression(
-					"=",
-					t.memberExpression(
-						t.identifier(varName),
-						t.identifier(assoc.block.selector.tag)),
-					parseAssociation(varName, assocElement, topPath))));
-
-		} else if (assoc.block.type == "If") {
-			let ifAssociations = [];
-			parseAssociations(varName, assoc, ifAssociations, topPath);
-			conditions.push({ cond: assoc.block.condition, child: t.blockStatement(ifAssociations) });
-
-			if (child.children.length > i+1) {
-
-				for (let y = i+1; y < child.children.length; y++) {
-					let next = child.children[y];
-					if (next.block.type == "Else") {
-						if (next.indent == assoc.indent) {
-							let elseAssociations = [];
-							parseAssociations(varName, next, elseAssociations, topPath);
-							conditions.push({ cond: null, child: t.blockStatement(elseAssociations) });
-							break;
-
-						}
-					} else if (next.block.type == "ElseIf") {
-
-						if (next.indent == assoc.indent) {
-							let elseIfAssociations = [];
-							parseAssociations(varName, next, elseIfAssociations, topPath);
-							conditions.push({ cond: next.block.condition, child: t.blockStatement(elseIfAssociations) });
-						}
-						else
-							break;
-
-					} else if (next.indent == assoc.indent) break;
-				}
-
-			}
-
-		} else if (assoc.block.type == "ElseIf" || assoc.block.type == "Else") {
-		} else {
-
-			throw topPath.hub.file.buildCodeFrameError(assoc.block.path, "invalid association element type " + assoc.block.type);
-
-		}
-
-		if (conditions.length > 0)
-			associations.push(generateIfChain(conditions, false));
-	}
-
-}
-
 export function parseSQLZ(path) {
 	let calls = parseCalls(path.node.right, path.hub.file),
 		orderedBlocks = orderBlocks(calls, path.hub.file),
@@ -184,4 +98,90 @@ export function parseSQLZ(path) {
 	}
 
 	path.replaceWithMultiple(declarations);
+}
+
+function parseAssociation(varName, assoc, topPath) {
+	let type = t.identifier(assoc.block.selector.tag),
+		target = t.memberExpression(
+			t.memberExpression(t.identifier("sequelize"), t.identifier('models')), 
+			t.identifier(assoc.block.selector.classes[0])),
+		options = t.objectExpression([]),
+		optionalOptions = [];
+
+	attributesToOptions(assoc, options);
+	childrenToOptions(assoc, options, optionalOptions, false, [], topPath);
+	let assocOpts = objOrAssign(options, optionalOptions);
+
+	let rslt = 
+		t.callExpression(
+			t.memberExpression(t.identifier(varName), type),
+			[ target, assocOpts ]);
+	return rslt;	
+}
+
+function parseAssociations(varName, child, associations, topPath) {
+
+	for (let i = 0; i < child.children.length; i++) {
+		let assoc = child.children[i];
+		let conditions = [];
+
+		if (assoc.block.type == "ElementBlock") {
+
+			associations.push(t.expressionStatement(parseAssociation(varName, assoc, topPath)));
+
+		} else if (assoc.block.type == "AssignmentExpression") {
+			let assocElement = orderBlocks([chainElement(assoc.block.path.right, topPath.hub.file)], topPath.hub.file)[0];
+			assocElement.children = assoc.children;
+
+			associations.push(t.expressionStatement(
+				t.assignmentExpression(
+					"=",
+					t.memberExpression(
+						t.identifier(varName),
+						t.identifier(assoc.block.selector.tag)),
+					parseAssociation(varName, assocElement, topPath))));
+
+		} else if (assoc.block.type == "If") {
+			let ifAssociations = [];
+			parseAssociations(varName, assoc, ifAssociations, topPath);
+			conditions.push({ cond: assoc.block.condition, child: t.blockStatement(ifAssociations) });
+
+			if (child.children.length > i+1) {
+
+				for (let y = i+1; y < child.children.length; y++) {
+					let next = child.children[y];
+					if (next.block.type == "Else") {
+						if (next.indent == assoc.indent) {
+							let elseAssociations = [];
+							parseAssociations(varName, next, elseAssociations, topPath);
+							conditions.push({ cond: null, child: t.blockStatement(elseAssociations) });
+							break;
+
+						}
+					} else if (next.block.type == "ElseIf") {
+
+						if (next.indent == assoc.indent) {
+							let elseIfAssociations = [];
+							parseAssociations(varName, next, elseIfAssociations, topPath);
+							conditions.push({ cond: next.block.condition, child: t.blockStatement(elseIfAssociations) });
+						}
+						else
+							break;
+
+					} else if (next.indent == assoc.indent) break;
+				}
+
+			}
+
+		} else if (assoc.block.type == "ElseIf" || assoc.block.type == "Else") {
+		} else {
+
+			throw topPath.hub.file.buildCodeFrameError(assoc.block.path, "invalid association element type " + assoc.block.type);
+
+		}
+
+		if (conditions.length > 0)
+			associations.push(generateIfChain(conditions, false));
+	}
+
 }
